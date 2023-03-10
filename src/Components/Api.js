9bd973753f28ad31bdbdfc487723f6e168c9406a
sagarjgb <sagarjgb@gmail.com>
2023-03-11 01:48:53 +0530

    videoBg

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -6,6 +6,7 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Table from "react-bootstrap/Table";
 import LoadingBox from "./LoadingBox";
+import videoBg1 from "../Videos/pexels-mati-mango-6330773.mp4";
 
 function Api() {
   const [data, setData] = useState(null);
@@ -24,47 +25,60 @@ function Api() {
 
   return (
     <>
-      <div className="d-flex flex-column site-container">
-        <Container>
-          <h1 className="text-center mt-4">Am I breached</h1>
-          <Form>
-            <InputGroup className="my-3">
-              {/* onChange for search */}
-              <Form.Control
-                onChange={(e) => setSearch(e.target.value)}
-                placeholder="Search name, title, domain"
-              />
-            </InputGroup>
-          </Form>
-          <Table striped bordered hover>
-            <thead>
-              <th>Name</th>
-              <th>Domain</th>
-            </thead>
-            <tbody>
-              {data ? (
-                data
-                  .filter((item) => {
-                    return search.toLowerCase() === ""
-                      ? item
-                      : item.Name.toLowerCase().includes(search);
-                  })
-                  .map((item) => (
-                    <tr key={item.Name}>
-                      <td>
-                        <Link className="link" to={`/moreinfo/${item.Name}`}>
-                          {item.Name}
-                        </Link>
-                      </td>
-                      <td>{item.Domain}</td>
-                    </tr>
-                  ))
-              ) : (
-                <LoadingBox />
-              )}
-            </tbody>
-          </Table>
-        </Container>
+      <div className="site-container">
+        <video src={videoBg1} autoPlay muted loop />
+        <div className="content">
+          <Container>
+            <div className="fixed-search">
+              <h1 className="text-center mt-4 heading">Am I breached</h1>
+              <p className="text-center white">
+                check whether the website is listed in data breach{" "}
+              </p>
+              <Form>
+                <InputGroup className="my-3">
+                  {/* onChange for search */}
+                  <Form.Control
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search name, title, domain"
+                  />
+                </InputGroup>
+              </Form>
+            </div>
+            <div className="overlay">
+              <Table hover>
+                <thead>
+                  <th>Name</th>
+                  <th>Domain</th>
+                </thead>
+                <tbody>
+                  {data ? (
+                    data
+                      .filter((item) => {
+                        return search.toLowerCase() === ""
+                          ? item
+                          : item.Name.toLowerCase().includes(search);
+                      })
+                      .map((item) => (
+                        <tr key={item.Name}>
+                          <td>
+                            <Link
+                              className="link"
+                              to={`/moreinfo/${item.Name}`}
+                            >
+                              {item.Name}
+                            </Link>
+                          </td>
+                          <td>{item.Domain}</td>
+                        </tr>
+                      ))
+                  ) : (
+                    <LoadingBox />
+                  )}
+                </tbody>
+              </Table>
+            </div>
+          </Container>
+        </div>
       </div>
     </>
   );
--- src/Components/MoreInfo.js
+++ src/Components/MoreInfo.js
@@ -6,14 +6,13 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Badge from "react-bootstrap/Badge";
 import LoadingBox from "./LoadingBox";
-import warningImage from "../Images/undraw_Warning_re_eoyh.png";
+import videoBg1 from "../Videos/pexels-mati-mango-6330773.mp4";
 
 export default function MoreInfo() {
   const [data, setData] = useState();
   const parms = useParams();
   const { name } = parms;
   const search = name;
-  console.log(name);
 
   useEffect(() => {
     async function fetchData() {
@@ -27,16 +26,62 @@ export default function MoreInfo() {
   }, []);
 
   return (
-    <div>
-      <Container fluid>
-        <h2 className="text-center">
-          <span className="danger">Danger</span>
-        </h2>
-        <Row>
-          <Col>
-            <img className="warning" src={warningImage} alt="Warning Image" />
-          </Col>
-          <Col>
+    <div className="site-container">
+      <video src={videoBg1} autoPlay muted loop />
+      <div className="content">
+        <Container fluid>
+          <h2 className="text-center">
+            <span className="danger">Danger</span>
+          </h2>
+          <Row>
+            <Col>
+              <h3 className="qna-q">
+                Have you used this website before the breach Date?
+              </h3>
+              <p className="qna-a">
+                <span>
+                  If, Yes!💀
+                  <br />{" "}
+                </span>
+                Then he knows <strong>your screte.</strong>
+              </p>
+            </Col>
+            <Col>
+              {data ? (
+                data
+                  .filter((item) => {
+                    return search.toLowerCase() === ""
+                      ? item
+                      : item.Name.toLowerCase().includes(search);
+                  })
+                  .map((item) => (
+                    <Row className="details">
+                      <Col className="details-col" key={item.Name}>
+                        <img
+                          className="logo d-logo"
+                          src={item.LogoPath}
+                          alt="Logo"
+                        />
+                        <p>{item.Name}</p>
+                        <a href={item.Domain}>{item.Domain}</a>
+                        <p>
+                          <span># </span>
+                          {item.PwnCount}
+                        </p>
+                        {item.DataClasses.map((data) => (
+                          <Badge bg="primary" className="badge">
+                            {data}
+                          </Badge>
+                        ))}
+                      </Col>
+                    </Row>
+                  ))
+              ) : (
+                <LoadingBox />
+              )}
+            </Col>
+          </Row>
+          <Row>
             {data ? (
               data
                 .filter((item) => {
@@ -45,52 +90,20 @@ export default function MoreInfo() {
                     : item.Name.toLowerCase().includes(search);
                 })
                 .map((item) => (
-                  <Row className="details">
-                    <Col className="details-col" key={item.Name}>
-                      <img
-                        className="logo d-logo"
-                        src={item.LogoPath}
-                        alt="Logo"
-                      />
-                      <p>{item.Name}</p>
-                      <a href={item.Domain}>{item.Domain}</a>
-                      <p>
-                        <span># </span>
-                        {item.PwnCount}
-                      </p>
-                      {item.DataClasses.map((data) => (
-                        <Badge bg="secondary" className="badge">
-                          {data}
-                        </Badge>
-                      ))}
+                  <>
+                    <Col>
+                      <span>Breach Date: </span>
+                      {item.BreachDate}
                     </Col>
-                  </Row>
+                  </>
                 ))
             ) : (
               <LoadingBox />
             )}
-          </Col>
-        </Row>
-        <Row>
-          {data ? (
-            data
-              .filter((item) => {
-                return search.toLowerCase() === ""
-                  ? item
-                  : item.Name.toLowerCase().includes(search);
-              })
-              .map((item) => (
-                <>
-                  <Col>
-                    <span>Breach Date: </span>{item.BreachDate}
-                  </Col>
-                </>
-              ))
-          ) : (
-            <LoadingBox />
-          )}
-        </Row>
-      </Container>
+          </Row>
+        </Container>
+        <h1 className="alert">Go Go Go.......... Secure yourself</h1>
+      </div>
     </div>
   );
 }
